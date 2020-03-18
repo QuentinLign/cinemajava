e83ab1415005b3c9b5a5d416ef31a5c1b9153d78
Quentin LIGNANI <48805997+QuentinLign@users.noreply.github.com>
2020-03-18 16:50:10 +0100

    Ajout BDD (à jour)

---
+++ BDD.java
@@ -0,0 +1,37 @@
+package interfacej;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import java.sql.Statement;
+
+
+public class BDD {
+//Connexion à la BDD
+public void insert(String nom) {
+String url="jdbc:mysql://localhost/restauration?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+String user="root";
+String password ="";
+//-->
+String name = "'RESTAURATION'";
+
+
+try {
+Connection cnx = DriverManager.getConnection(url,user,password);
+Statement stmt = cnx.createStatement();
+//Execution des requêtes
+       ResultSet rs;    
+       stmt.executeUpdate("INSERT INTO reservations (nom, classe, jours, DP, regimealimentaire, somme, role)" + "VALUES ('"+nom+"'");
+       System.out.println("insertion reussie !");
+
+       //Message d'erreur
+           cnx.close();
+}catch (SQLException e) {
+System.out.println("Une erreur est survenue lors de la connexion a la base de données");
+e.printStackTrace();
+
+}
+
+}
+}
