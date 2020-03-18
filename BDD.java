25489e483c806791509ddd9b35dee72e0644f20f
Quentin LIGNANI <48805997+QuentinLign@users.noreply.github.com>
2020-03-18 12:08:25 +0100

    BDD à jour : nom,jours,DP,regimealimentaire,somme

--- BDD.java
+++ BDD.java
@@ -1,4 +1,4 @@
-package cinema;
+package restauration;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -8,27 +8,27 @@ import java.sql.Statement;
 
 
 public class BDD {
-//Connexion à la BDD
-public void insert(String Etudiant) {
-String url="jdbc:mysql://localhost/cinema?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+//Connexion Ã  la BDD
+public void insert(String nom) {
+String url="jdbc:mysql://localhost/restauration?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 String user="root";
 String password ="";
 //-->
-String nom = "'CINEMA'";
+String name = "'CINEMA'";
 
 
 try {
 Connection cnx = DriverManager.getConnection(url,user,password);
 Statement stmt = cnx.createStatement();
-//Execution des requêtes
+//Execution des requÃªtes
        ResultSet rs;    
-       stmt.executeUpdate("INSERT INTO reservations (numero_salle, film, etudiant, dixans, navigo, pleintarif)" + "VALUES ('"+Etudiant+"'");
+       stmt.executeUpdate("INSERT INTO reservations (nom, jours, DP, regimealimentaire, somme)" + "VALUES ('"+nom+"'");
        System.out.println("insertion reussie !");
 
        //Message d'erreur
            cnx.close();
 }catch (SQLException e) {
-System.out.println("Une erreur est survenue lors de la connexion a la base de données");
+System.out.println("Une erreur est survenue lors de la connexion a la base de donnÃ©es");
 e.printStackTrace();
 
 }
